5ea87473984800c70e19852e88043bc4e7a51f56
LaxWasHere <public@LaxWasHere.com>
2013-04-29 07:44:59 -0700

    1.4

--- src/net/lazlecraft/spawntp/SpawnTP.java
+++ src/net/lazlecraft/spawntp/SpawnTP.java
@@ -29,36 +29,47 @@ public class SpawnTP extends JavaPlugin implements Listener {
 		getConfig().options().copyDefaults(true);
 		this.saveConfig();
 		//Get config values.
-		sYaw = getConfig().getInt("SpawnYaw");
-		sPitch = getConfig().getInt("SpawnPitch");
-		sX = getConfig().getDouble("SpawnX");
-		sY = getConfig().getDouble("SpawnY");
-		sZ = getConfig().getDouble("SpawnZ");
-		sWorld = getConfig().getString("SpawnWorld");
+		confreload();
+		//Register events
+		getServer().getPluginManager().registerEvents(this, this); 
 		//Metrics
-		getServer().getPluginManager().registerEvents(this, this); try {
+		try {
                     Metrics metrics = new Metrics(this);
                     metrics.start();
         }
                 catch (IOException e) {}
 	} 
 	
+	public void confreload() {
+		sYaw = getConfig().getInt("SpawnYaw");
+		sPitch = getConfig().getInt("SpawnPitch");
+		sX = getConfig().getDouble("SpawnX");
+		sY = getConfig().getDouble("SpawnY");
+		sZ = getConfig().getDouble("SpawnZ");
+		sWorld = getConfig().getString("SpawnWorld");
+	}
+
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage("You may not use SpawnTP commands, console!");
             return false;
         }
+             
     	Player p = (Player)sender;
         if (((commandLabel.equalsIgnoreCase("setspawn")) || commandLabel.equalsIgnoreCase("sss")) && (sender.hasPermission("spawntp.setspawn"))) {
         	Location l = p.getLocation();
-        	getConfig().set("SpawnX", Integer.valueOf(l.getBlockX()));
-        	getConfig().set("SpawnY", Integer.valueOf(l.getBlockY()));
-        	getConfig().set("SpawnZ", Integer.valueOf(l.getBlockZ()));
+        	int x = l.getBlockX();
+        	int y = l.getBlockY();
+        	int z = l.getBlockZ();
+        	getConfig().set("SpawnX", Double.valueOf(l.getBlockX() + 0.5));
+        	getConfig().set("SpawnY", Double.valueOf(l.getBlockY() + 0.5));
+        	getConfig().set("SpawnZ", Double.valueOf(l.getBlockZ() + 0.5));
         	getConfig().set("SpawnYaw", Float.valueOf(l.getYaw()));
         	getConfig().set("SpawnPitch", Float.valueOf(l.getPitch()));
         	getConfig().set("SpawnWorld", String.valueOf(l.getWorld().getName()));
-        	saveConfig();
+        	p.getWorld().setSpawnLocation(x, y, z);
         	p.sendMessage(prefix + ChatColor.GREEN + "Spawn set!");
+        	confreload();
         }
         else if (commandLabel.equalsIgnoreCase("spawn") && (sender.hasPermission("spawntp.spawn"))) {
         	Location SpawnLoc = new Location(Bukkit.getServer().getWorld(sWorld), sX, sY, sZ, sYaw, sPitch);
@@ -66,16 +77,21 @@ public class SpawnTP extends JavaPlugin implements Listener {
         }
         else if (commandLabel.equalsIgnoreCase("spawntp")) {
         	p.sendMessage(prefix + ChatColor.GOLD + "This plugin is made by the almighty LaxWasHere");
-        	p.sendMessage(prefix + "Running version " + this.getDescription().getVersion());
+        	p.sendMessage(prefix + "Running version " + ChatColor.RED + this.getDescription().getVersion());
+        	confreload();
         }
-        return false;
+        return true;
 	}
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent ev) {
 		Player p = ev.getPlayer();
+		if (sWorld == null)
+			p.teleport(p.getWorld().getSpawnLocation().add(0.5,0.5,0.5));
+		else {
 			Location SpawnLoc = new Location(Bukkit.getServer().getWorld(sWorld), sX, sY, sZ, sYaw, sPitch);
 			p.teleport(SpawnLoc);		
+		}
 	}
 }
 //LaxWasHere
